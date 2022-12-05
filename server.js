113564cc6511d099d683b47c3430fc70db8e34a4
abubakar-hayat <abubakar.hayat@devsinc.com>
2022-12-05 20:19:13 +0500

    Added some code

--- server.js
+++ server.js
@@ -14,10 +14,16 @@ app.use(cors())
 
 app.use(express.json({ limit: '2mb', extended: true}))
 app.use(express.urlencoded({limit: "2mb", extended: true, parameterLimit: 50000}))
-app.use(express.static(path.join(__dirname, 'public')))
-app.get('/', (req, res) => {
-  res.json({msg: 'Welcome to twitter!'})
-})
+
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./client/build/index.html"),
+    function (err) {
+      res.status(500).send(err);
+    }
+  );
+});
 
 app.use('/tweets', tweetRoutes)
 
